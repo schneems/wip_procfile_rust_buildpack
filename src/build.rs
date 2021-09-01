56f49a9042be4dffb8ce6d76949e8e4e528d26d6
Richard Schneeman <rschneeman@salesforce.com>
2021-09-01 15:40:04 -0500

    Manually write launch layer.toml
    Works 🤷‍♂️

--- src/build.rs
+++ src/build.rs
@@ -11,21 +11,28 @@ pub fn build(
     // let source = context.buildpack_dir.join("opt").join("run.sh");
     let procfile_path = context.app_dir.join("Procfile");
     let contents = fs::read_to_string(procfile_path).unwrap();
-    // println!("{}", procfile_path);
-    // println!("{}", contents.unwrap());
 
+
+    fs::create_dir_all(context.layers_dir.join("procfile"));
+    fs::write(
+        context.layers_dir.join("launch.toml"),
+        "
+[[processes]]
+type = \"web\"
+command = \"while true; do echo 'lol'; sleep 2; done\"
+");
     println!("{}", contents.as_str());
 
-    let launch = Launch::default().process(Process::new(
-        "web",
-        contents,
-        vec![String::from("")],
-        false,
-    )?);
-
-    context
-        .write_launch(launch)
-        .map_err(JvmFunctionInvokerBuildpackError::CouldNotWriteLaunchToml)?;
+    // let launch = Launch::default().process(Process::new(
+    //     "web",
+    //     contents,
+    //     vec![String::from("")],
+    //     false,
+    // )?);
+    //
+    // context
+    //     .write_launch(launch)
+    //     .map_err(JvmFunctionInvokerBuildpackError::CouldNotWriteLaunchToml)?;
 
     Ok(())
 }
